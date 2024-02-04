c8c9c0850308c0810845fc11cba89c253192ebf9
msrahman4 <158791385+msrahman4@users.noreply.github.com>
2024-02-04 08:40:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 
 streamlit.title('In the name of Allah. The most beneficient, the most merciful!')
 
-All praises be to you, Oh Lord of the world and the Hereafter. Make me and my Parents, Siblings, my wife and my kids to be amongst the people whome you have shown your mercy and favor! Ameen.
+streamlit.text('All praises be to you, Oh Lord of the world and the Hereafter'.)
+streamlit.text('Make me and my Parents, Siblings, my wife and my kids to be amongst the people whome you have shown your mercy and favor! Ameen.')
