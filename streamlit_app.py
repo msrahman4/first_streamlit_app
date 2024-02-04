f166e25319ed00a3ef51ac3bd27ccbb779f676f5
msrahman4 <158791385+msrahman4@users.noreply.github.com>
2024-02-04 08:48:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ streamlit.title('In the name of Allah. The most beneficient, the most merciful!'
 streamlit.header('O Lord of the world, give unto me and my kids - more Knowledge.')
 streamlit.text('All praises be to you, Oh Lord of the world and the Hereafter.')
 streamlit.text('Make me and my Parents, Siblings, my wife and my kids to be amongst the people whome you have shown your mercy and favor. Ameen')
+streamlit.text('Insha Allah')
